1efa38bfbd3667a97ab419007bdfadfcd2f9c8aa
Santiago Pérez <xantiago.spa@gmail.com>
2026-03-09 10:17:48 +0100

    nuevocambio

--- sap_nuevo_selenium.py
+++ sap_nuevo_selenium.py
@@ -27,19 +27,59 @@
 #una vez dentro solo apareceran los hitos modificables
 # aparecera un campo llamado modificar fecha real , le haces clik y automaticamente se modifican todas las fechas reales de los hitos seleccionados
 # le haces click a Guardar
-
-
-
+# IMPORTANTE: Asegurarse de que el WebDriver y el navegador estén actualizados y sean compatibles.
+# Requiere instalar las siguientes librerías:
+# pip install selenium webdriver-manager
+# pip install selenium-stealth
+# pip install selenium-wire
+# pip install undetected-chromedriver
+# pip install pyautogui
 
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-# Configuración del navegador
-options = Options()
-options.add_argument("--start-maximized")  # Iniciar maximizado
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+def main():
+    # Configuración del navegador
+    options = Options()
+    options.add_argument("--start-maximized")
+    options.add_argument("--disable-infobars")
+    options.add_argument("--disable-extensions")
+    
+    # Inicializar el WebDriver
+    driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+    
+    try:
+        # Abrir la página de SAP
+        driver.get("https://fm21global.tg.telefonica/fiori?sap-client=550&sap-language=ES#ZOBJ_Z_GESTION_HITOS_0001-display?sap-ie=edge&sap-theme=sap_belize&sap-touch=0")
+        
+        # Esperar a que la página cargue completamente
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, "M0:46:::1:34-r")))
+        
+        # Ingresar el nombre/numero del proyecto
+        project_input = driver.find_element(By.ID, "M0:46:::1:34-r")
+        project_input.send_keys("Nombre o numero del proyecto")  # Reemplazar con el valor real
+        
+        # Hacer click en el botón de ejecutar
+        execute_button = driver.find_element(By.ID, "M0:50::btn[8]-r")
+        execute_button.click()
+        
+        # Esperar a que se abra la nueva pestaña con los hitos del proyecto
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "sapMListTbl")))
+        
+        # Buscar el hito/hitos que quieres modificar (reemplazar con el criterio de búsqueda real)
+        milestone_code = "Codigo del hito"  # Reemplazar con el código real del hito
+        milestone_row = driver.find_element(By.XPATH, f"//tr[td[contains(text(), '{milestone_code}')]]")
+        
+        # Seleccionar el hito haciendo click en la columna "x"
+        select_checkbox = milestone_row.find_element(By.XPATH, ".//td[1]//input[@type='checkbox']")
+        select_checkbox.click()
+        
+        # Hacer click en Modificar
+        modify_button = driver.find_element(By.ID, "M0:50::btn[9]-r")  # Reemplazar con el ID correcto del botón Modificar
+        modify_button.click()
